edd34133baab7e742c01304dc47b830357c8298d
unknown <Operaciones@Operaciones099.ad.buenosaires.gob.ar>
2023-05-17 00:57:43 -0300

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'David Dash')
+
+@section('content_header')
+    <h1>Inicio de un nuevo Sueño</h1>
+@stop
+
+@section('content')
+    <p>Que no decaigaaa.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
